63b7f4a415859618fe52ae394a604ad93d87880c
Marien M <marien.moutima-backenga@edu.devinci.fr>
2023-11-03 14:23:38 +0100

    Add of Pages

---
+++ Front/Pages/login.jsx
@@ -0,0 +1,9 @@
+export default function Login() {
+    return (
+      <div className="login-form">
+          <input type="text" placeholder="Username" />
+          <input type="password" placeholder="Password" />
+          <button type="submit">Login</button>
+      </div>
+    )
+  } 
