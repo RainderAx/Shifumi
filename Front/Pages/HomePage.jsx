80cbe9e36f6480ef501b0f2f7cdeb976d9ba175e
RainderAx <114318268+RainderAx@users.noreply.github.com>
2024-01-11 19:01:17 +0100

    CSS

--- Front/Pages/HomePage.jsx
+++ Front/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Login from '../Pages/login'
 
-import './HomePage.css';
+import './Components&Style/HomePage.css';
 
 const handleRedirect = () => {
   window.location.href = "https://www.karminecorp.fr/shop";
@@ -14,9 +14,9 @@ const TestPage = () => {
   return (
     
     <div>
-       <button className='main-button'><a href="/login" className='link-button-play'>Jouer</a></button>*
        <h1>C'est la maison du content</h1>
-       <button onClick={handleRedirect} className="btn">Nah I'd Win</button>
+       <button onClick={handleRedirect} >Nah I'd Win</button>
+       <button className='main-button'><a href="/login" className='link-button-play'>Jouer</a></button>
        
     </div>
     
