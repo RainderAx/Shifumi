d5f76ea8513916e6da24f90832d0a19363638677
RainderAx <114318268+RainderAx@users.noreply.github.com>
2024-01-11 19:06:54 +0100

    letmecook

--- Front/Pages/HomePage.jsx
+++ Front/Pages/HomePage.jsx
@@ -1,16 +1,26 @@
-// Remove the import statement for React since it is not being used
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Login from '../Pages/login'
+
+import './Components&Style/HomePage.css';
 
 const handleRedirect = () => {
-    window.location.href = 'https://www.karminecorp.fr/shop';
+  window.location.href = "https://www.karminecorp.fr/shop";
 };
 
 const TestPage = () => {
-    return (
-        <div>
-            <h1>Home Page</h1>
-            <button onClick={handleRedirect}>Go to Website</button>
-        </div>
-    );
+
+
+  return (
+    
+    <div>
+       <h1>C'est la maison du content</h1>
+       <button onClick={handleRedirect} >Nah I'd Win</button>
+       <button className='main-button'><a href="/login" className='link-button-play'>Jouer</a></button>
+       
+    </div>
+    
+  );
 };
 
 export default TestPage;
