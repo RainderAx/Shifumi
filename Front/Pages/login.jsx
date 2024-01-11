21c1209b9c67f90526b18409ce4adfe99a2c0d8d
Marien M <marien.moutima-backenga@edu.devinci.fr>
2024-01-11 16:13:49 +0100

    Meilleur login

--- Front/Pages/login.jsx
+++ Front/Pages/login.jsx
@@ -1,16 +1,62 @@
+import { useState } from 'react';
 
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
+  // Fonction pour gérer le changement de l'input username
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
 
+  // Fonction pour gérer le changement de l'input password
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
 
-export default function Login() {
-    return (
-      <div className="login-form">
-          <input type="text" placeholder="Username" />
-          <input type="password" placeholder="Password" />
-          <button type="submit">Login</button>
-      </div>
-    )
-  } 
+  // Fonction pour gérer la soumission du formulaire
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
+    // Vérifier les informations de connexion
+    if (username === 'admin' && password === 'password') {
+      console.log('Connexion réussie');
+      
+    } else {
+      console.log('Identifiant ou mot de passe incorrect');
+      setError('Identifiant ou mot de passe incorrect'); 
+    }
+  };
 
-  
+  const [error, setError] = useState('');
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <p>{error}</p>} {"erreur de message, pas le bon mdp pd"}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+
+}
+export default Login;
