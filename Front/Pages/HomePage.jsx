015e19a999501c76bb2153e8f8f5e133973cb89f
RainderAx <114318268+RainderAx@users.noreply.github.com>
2024-01-12 19:45:49 +0100

    Logo

--- Front/Pages/HomePage.jsx
+++ Front/Pages/HomePage.jsx
@@ -1,37 +1,52 @@
+import React, { useState } from 'react';
 import './Components&Style/HomePage.css';
 import { ButtonBase } from './Components&Style/Button';
-import React, { useState } from 'react';
-
+import luffy from '../src/assets/luffy.png';
+import naruto from '../src/assets/naruto.png';
+import goku from '../src/assets/goku.png';
 
 const TestPage = () => {
-  const [texte, setTexte] = useState('ShifumiGame'); 
+  const [texte, setTexte] = useState('ShifumiGame');
 
   const changerTexteAuSurvole = () => {
-    setTexte("C'est la maison du content"); 
+    setTexte("C'est la maison du content");
   };
 
   const reinitialiserTexte = () => {
-    setTexte('ShifumiGame'); 
+    setTexte('ShifumiGame');
   };
+
+  const GameRules = [
+    { logo: goku, title: 'Goku', description: 'Goku bat Luffy' },
+    { logo: luffy, title: 'Luffy', description: 'Luffy bat Naruto' },
+    { logo: naruto, title: 'Naruto', description: 'Naruto bat Goku' },
+  ];
 
   return (
     <div className='container'>
       <div>
-        <h1
-          onMouseOver={changerTexteAuSurvole}
-          onMouseOut={reinitialiserTexte}>
+        <h1 onMouseOver={changerTexteAuSurvole} onMouseOut={reinitialiserTexte}>
           {texte}
         </h1>
       </div>
 
-      <div className='animation'>       
-        <ButtonBase variant="main">
-          <a href="/">Jouer</a>
+      <div className='animation'>
+        <ButtonBase variant='main'>
+          <a href='/'>Jouer</a>
         </ButtonBase>
-      </div>    
-
+      </div>
       
-        
+      <div className='regles'>
+        {GameRules.map((rule, index) => (
+          <div key={index}>
+            <img className='logo' src={rule.logo} alt={rule.title} />
+            <h2>{rule.title}</h2>
+            <p>{rule.description}</p>
+          </div>
+        ))}
+      </div>
+
+
     </div>
   );
 };
