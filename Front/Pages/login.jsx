693da2df9cda78a8b5cec312fef3ef81685b9018
RainderAx <114318268+RainderAx@users.noreply.github.com>
2024-01-13 22:01:19 +0100

    Probleme Requete

--- Front/Pages/login.jsx
+++ Front/Pages/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { loginUser} from '../user-fetch';
+import { loginUser} from '../src/components/Api/Login-user';
 
 
 const Login = () => {
--- Front/src/components/Api/Login-user.js
+++ Front/src/components/Api/Login-user.js
@@ -1,4 +1,8 @@
-import { keepTokenAndUsername } from "./Local-token";
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+import { keepTokenAndUsername } from './Local-token';
+
+const API_URL = 'http://fauques.freeboxos.fr:3000';
 
 export const loginUser = async (username, password) => {
     try {
@@ -22,4 +26,17 @@ export const loginUser = async (username, password) => {
       throw error;
     }
 };
-
+export async function registerUser(username, password) {
+  const idUser = uuidv4();
+  try {
+    const response = await axios.post(`${API_URL}/register`, { id_: idUser, username, password });
+    if (response.data.success) {
+      return { status: 'success', data: response.data };
+    } else {
+      throw new Error('Registration failed');
+    }
+  } catch (error) {
+    console.error('Registration error:', error);
+    return { status: 'error', message: error.message };
+  }
+}
